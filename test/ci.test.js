8031ce56c529e6c4eea80131268162e6169fd1c2
Winterreisender <winterreisender@foxmail.com>
2023-06-24 09:57:30 +0800

    add ci

---
+++ test/ci.test.js
@@ -0,0 +1,11 @@
+/**
+ * This is the test for CI (GitHub Actions)
+ */
+const { Webview } = require('webview-nodejs');
+
+function main() {
+    let w = new Webview();
+}
+
+import {test} from '@jest/globals'
+test('ci',main);
